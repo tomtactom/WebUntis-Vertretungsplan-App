e28562ed006762a712cc2d4172c233e947bcac93
Tom <32821177+tomtactom@users.noreply.github.com>
2021-01-11 16:44:47 +0100

    Update untis.rm.inc.php

--- untis.rm.inc.php
+++ untis.rm.inc.php
@@ -1,8 +1,8 @@
 <?php
   // Benenne diese Datei um (untis.inc.php) und vervollständige den Inhalt:
 
-	#$hash = hash('sha512', 'd[0<~]PH'.'V3rtr3tung-42'.'94j|i4BY');
-	$hash = '2938324fa26cf8716bbb74edae6224bad017d5a6bf65a232ecf9e99d0d6aac57eec7f7f523437502f23c28d09f2c11abd31d819535a50a3e8d5dd63ecbb13c6f'; // V3rtr3tung-42   // Bitte ändere das Passwort sobald es geht!
+	#$hash = hash('sha512', 'salt'.'passwort'.'salt');
+	$hash = '???'; // passwort   // Bitte ändere das Passwort sobald es geht!
 	# Untis Login Daten in Datei ./untis.inc.php eingeben
 	$schooldomain = ''; // Gebe die Schul-Internetseite ein, über die WebUntis läuft
 	$schoolname = ''; // Gebe den Schulnamen ein, über den WebUntis läuft
